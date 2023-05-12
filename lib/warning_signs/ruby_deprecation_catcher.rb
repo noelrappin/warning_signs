5e88247ecc5b1668eba357ddbeaba0092e006b74
Noel Rappin <noelrap@hey.com>
2023-05-12 10:06:45 -0500

    Still weirdness in the server

--- lib/warning_signs/ruby_deprecation_catcher.rb
+++ lib/warning_signs/ruby_deprecation_catcher.rb
@@ -2,6 +2,8 @@ module WarningSigns
   module RubyDeprecationCatcher
     include CallerLocationHelper
     def warn(message, category: nil)
+      ap caller_locations
+      ap caller_location_start
       Deprecation.new(
         augmented_message(message, category),
         source: "ruby",
