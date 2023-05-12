5edf5420a3d2bffa6ec069c0339b68015c36c2cd
Noel Rappin <noelrap@hey.com>
2023-05-12 09:44:27 -0500

    Clean up for vendor path

--- lib/warning_signs/caller_location_helper.rb
+++ lib/warning_signs/caller_location_helper.rb
@@ -1,10 +1,9 @@
 module WarningSigns
   module CallerLocationHelper
     def caller_location_start
-      ap caller_locations.map { |location| location.to_s }
       caller_locations.find_index do |location|
         !location.to_s.include?("<internal:") &&
-          !location.to_s.include?("warning_signs") &&
+          !location.to_s.include?("warning_signs/lib") &&
           !location.to_s.include?("rubygems") &&
           !location.to_s.include?("/gems")
       end || 0
