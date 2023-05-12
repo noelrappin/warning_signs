fa68abb7852381bf17be1a7d7872da8966886459
Noel Rappin <noelrap@hey.com>
2023-05-12 09:40:08 -0500

    Testing

--- lib/warning_signs/caller_location_helper.rb
+++ lib/warning_signs/caller_location_helper.rb
@@ -1,6 +1,7 @@
 module WarningSigns
   module CallerLocationHelper
     def caller_location_start
+      ap caller_locations.map { |location| location.to_s }
       caller_locations.find_index do |location|
         !location.to_s.include?("<internal:") &&
           !location.to_s.include?("warning_signs") &&
